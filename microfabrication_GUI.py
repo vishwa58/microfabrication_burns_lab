68a9674d293b1794e93e50eac327e1d9f40239eb
Vishwa Nathan <vnathan@umich.edu>
2021-03-27 17:30:18 -0400

    fixed after function

--- microfabrication_GUI.py
+++ microfabrication_GUI.py
@@ -2,16 +2,21 @@ import tkinter as tk
 import microfabrication_functions as mfc
 import microfabrication_constant as mfc_const
 import  time as time
+from datetime import datetime, timedelta
+import threading
+import image_display_2
 #from PIL import Image, ImageTk
 # img = Image.open("flower.png")
 # img = img.resize((34, 26), Image.ANTIALIAS)
 
-
+TOTAL_TIME=0
+START=0
+END=0
+lister =[]
+#timon=threading.Timer(mfc_const.DISPLAY_TIME, lambda: scroll_images(parental_figure, image_list, tkinter_label) ).start()
 
 image_filelist = mfc.read_files(mfc_const.IMAGE_PATH) #This list holds the names of all the images nd is sorted by slice num
-
-
-
+print(len(image_filelist))
 
 
 def set_text(text, entrybox):
@@ -23,11 +28,71 @@ def set_text(text, entrybox):
 #This function takes in a list of images and a label and will place the image in the center of the screen for a proper amt of time
 
 #This function will have to be fixed in the future to incorporate the new stuff
-def cycle_images(parental_figure, image_list, tkinter_label):
+def scroll_images(parental_figure, image_list, tkinter_label):
+    global END, START
+    # filename = image_list.pop(0)
+    # fullpath = mfc_const.IMAGE_PATH+filename
+    # display_image(parental_figure,fullpath , tkinter_label)
     if not image_list:
+        END= time.time()
+        avg_time = (END-START)#/36
+        print(avg_time)
+        mw.quit()
+        return
+
+    filename = image_list.pop(0)
+    pre_exp_image = tk.PhotoImage(file = (mfc_const.IMAGE_PATH+filename.original_filename))
+    exposure = pre_exp_image
+    tkinter_label.config(image = exposure, highlightthickness=0, borderwidth =0, relief ="flat")
+    tkinter_label.image = exposure
+    tkinter_label.place(relx=0.5, rely=0.5, anchor = 'c')
+    threading.Timer(mfc_const.DISPLAY_TIME, lambda: scroll_images(parental_figure, image_list, tkinter_label) ).start()
+
+
+
+
+
+
+
+
+
+
+
+def cycle_images(parental_figure, image_list, tkinter_label, display_time):
+    # x=0
+    # while(len(image_list)!=0):
+    #     filename = image_list.pop(0)
+    #     print("popped "+ filename.original_filename)
+    #     pre_exp_image = tk.PhotoImage(file = (mfc_const.IMAGE_PATH+filename.original_filename))
+    #     exposure = pre_exp_image
+    #     tkinter_label.config(image = exposure, highlightthickness=0, borderwidth =0, relief ="flat")
+    #     tkinter_label.image = exposure
+    #     start=time.time()
+    #     end_time=start+mfc_const.DISPLAY_TIME
+    #     tkinter_label.place(relx=0.5, rely=0.5, anchor = 'c')
+    #     # while(time.time()<end_time):
+    #     #     #print(x)
+    #     #     print("gang")
+    #     #     tkinter_label.place(relx=0.5, rely=0.5, anchor = 'c')
+    #     #     #x = x+1
+
+        
+
+
+
+        
+    if not image_list:
+        #print(time.time())
+        # END = time.time()
+        # TOTAL_TIME=START-END
+        for elem in lister:
+            print(elem)
+        mw.quit()
+
+        #showframe(driverwindow)
         return
     filename = image_list.pop(0)
-    print("popped "+ filename.original_filename)
+    #print("popped "+ filename.original_filename)
     pre_exp_image = tk.PhotoImage(file = (mfc_const.IMAGE_PATH+filename.original_filename))
     #pre_exp_image = Image.open(mfc_const.IMAGE_PATH+filename.original_filename)
     #pre_exp_image=pre_exp_image.resize((mfc_const.IMAGE_SIZE_X, mfc_const.IMAGE_SIZE_Y), Image.ANTIALIAS)
@@ -39,37 +104,20 @@ def cycle_images(parental_figure, image_list, tkinter_label):
     # exposure.resize((mfc_const.IMAGE_SIZE_X, mfc_const.IMAGE_SIZE_Y), Image.ANTIALIAS)
     tkinter_label.config(image = exposure, highlightthickness=0, borderwidth =0, relief ="flat")
     tkinter_label.image = exposure
+    #start = time.time()
     tkinter_label.place(relx=0.5, rely=0.5, anchor = 'c')
-    parental_figure.after(50, lambda: cycle_images(parental_figure, image_filelist, tkinter_label))
+    START=time.time()
+    parental_figure.after(mfc_const.cyc_DISPLAY_TIME, lambda: cycle_images(parental_figure, image_filelist, tkinter_label, mfc_const.cyc_DISPLAY_TIME))
+    END=time.time()-START
+    lister.append(END)
+    # print(END)
+    # end = time.time()-start
+    # print(end)
 
 
 
 
 
-    # exposure = tk.PhotoImage(file = (mfc_const.IMAGE_PATH+filename.original_filename))
-    # new_exposure=resize_image(mfc_const.IMAGE_SIZE_X, mfc_const.IMAGE_SIZE_Y, mfc_const.IMAGE_PATH+filename.original_filename )
-    # tkinter_label.config(image = new_exposure)
-    # tkinter_label.image = new_exposure
-    # tkinter_label.place(relx=0.5, rely=0.5, anchor ='c')
-    # parental_figure.after(1, lambda: cycle_images(parental_figure, image_filelist, tkinter_label))
-    #parental_figure.after(500, cycle_images(parental_figure, image_filelist, tkinter_label))
-
-    
-
-
-    # for filename in image_list:#loops throguh differnt files
-    #     exposure = tk.PhotoImage(file = (mfc_const.IMAGE_PATH+filename.original_filename))
-    #     tkinter_label.config(image = exposure)
-    #     # central_image = tk.Label(self, text = "image goes here", image = exposure)
-    #     tkinter_label.image =exposure
-    #     t_end = (time.time())+(60.0/1000)
-    #     print("prob an infitinte loop")
-    #     x =3
-    #     while((time.time())<=t_end):
-    #         print(time.time())
-    #         print(t_end)
-    #         print('def an infinite loop')
-    #         tkinter_label.place(relx=0.5, rely=0.5, anchor ='c')
   
 
 
@@ -141,11 +189,14 @@ class driverwindow(tk.Frame):
         # invalidate_button = tk.Button(self, text = "Invalidate", command = lambda: controller.show_frame("open_window"))
         # invalidate_button.pack()
         #cycle_images(parent, image_filelist, self.central_image)
-
-        run_button = tk.Button(self, text = "run fast", command = lambda: [run_button.pack_forget(),cycle_images(parent, image_filelist, self.central_image)] )
+        
+        #run_button = tk.Button(self, text = "run fast", command = lambda: [run_button.pack_forget(),cycle_images(parent, image_filelist, self.central_image, mfc_const.cyc_DISPLAY_TIME)] )
+        run_button = tk.Button(self, text = "run fast", command = lambda:[run_button.pack_forget(), start_timer(), scroll_images(parent, image_filelist, self.central_image)])
         run_button.pack()
 
-        
+def start_timer():
+    global START
+    START = time.time()     
 
 # def cycle_images(parental_figure, image_list, tkinter_label):
 #     if not image_list:
@@ -153,7 +204,7 @@ class driverwindow(tk.Frame):
 #     filename = image_list.pop(0)
 #     print("popped "+ filename.original_filename)
 #     exposure = tk.PhotoImage(file = (mfc_const.IMAGE_PATH+filename.original_filename))
-#     tkinter_label.config(image = exposure)
+#     tkinter_label.config(image = exposure), 
 #     tkinter_label.place(relx=0.5, rely=0.5, anchor ='c')
 #     parental_figure.after(1, lambda: cycle_images(parental_figure, image_filelist, tkinter_label))
 
@@ -179,4 +230,7 @@ mw =  window_controller()
 #tenthtop = tk.PhotoImage(file = "basket.png")
 #mw.show_frame("open_window")
 # mw.after(500, cycle_images)
+
 mw.mainloop()
+# for elem in lister:
+#     print(elem)
